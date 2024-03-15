4c751f1c9d17f7f08024503d40cda08e42cba1db
Aman Boora <146092218+boora8181@users.noreply.github.com>
2024-03-15 09:06:06 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+// let score ="aman"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+
+// "33"=> 33
+// 33abc => NaN ( Not a number)
+// null => 0
+// true => 1, false=> 0
+
+let isLoggedIn = "Aman"
+console.log(typeof isLoggedIn);
+let bolleanisisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof bolleanisisLoggedIn);
+
+// "Aman"=> true
+// 1=> true, 0=> false
+// " "=> false
