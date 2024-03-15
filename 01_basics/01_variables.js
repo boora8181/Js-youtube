7e37001ef61e43d618ad429ad144deffe523df77
Aman Boora <146092218+boora8181@users.noreply.github.com>
2024-03-15 07:33:34 +0000

    learn new topic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =12345
+let accountemail ="aman@.com"
+var accountpwd ="0987654321"
+accountcity = "Hisar"
+let accountstate;
+
+// accountId =54321 this is not allowed kunki ye ek constant h 
+
+accountemail= "1234@.com"
+accountpwd = "1234567890"
+accountcity = "panipat"
+
+//const or let is use karna h var nhi use karna because of issue in functional scope and block scope {} ise scope khte h 
+
+console.table([ accountId, accountemail, accountpwd, accountcity, accountstate]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all Js code as newer version 
+
+// datatypes
+
+// number => 2 to power of 55
+// bigint => for bigger numbers in 
+// string => " "
+// Boolean = True/ false
+// null => isme hum value of null bol dete h iska type object aayega 
+// undefined => isme value hi undefined hoti h 
+// symbol => unique 
+
+console.log(typeof undefined) // returns undefined type 
+
+console.log(typeof null) // returns object type 
+
+// typeof se hami kisi bi datatype ka type dekh sakte h 
+
