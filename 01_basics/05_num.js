f6f581ca51468564f6f8b2faa3a61eb15ec591ee
Aman Boora <146092218+boora8181@users.noreply.github.com>
2024-03-16 05:10:58 +0000

    Math and Num

---
+++ 01_basics/05_num.js
@@ -0,0 +1,37 @@
+const score= 400
+// console.log(score);
+
+const newbalance = new Number(100)
+// console.log(newbalance);
+
+
+// console.log(newbalance.toString().length);
+// console.log(newbalance.toFixed(1));
+
+const othernumber = 11.995
+// console.log(othernumber.toPrecision(3));
+
+const hundreds = 10000000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+/*****          Maths           *******/
+
+// console.log(Math);
+// console.log(Math.abs(-19));
+// console.log(Math.round(9.796));
+// console.log(Math.ceil(4.1));   // isme near greatest integer dega jaise 4.1 ka bi 5 hi dia h 4.3 ho ya 4.9 sbka 5 hi dega 
+// console.log(Math.floor(4.9));   // isme lowest near integer dega jaise 4.9 ka 4 hi dia h aise hi isme 4 hi dega sabka jo bi 4-5 ke bitch me h 
+
+// console.log(Math.min(1,2,6,9,5));
+// console.log(Math.max(1,9,10,2,));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) +1 );
+
+
+const min= 1
+const max = 99
+console.log((Math.random()*( max-min + 1)) + min);
+
+
