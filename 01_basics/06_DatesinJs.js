4843ff62adaa66b7296013ff1927a72e11e9763c
Aman Boora <146092218+boora8181@users.noreply.github.com>
2024-03-16 06:16:34 +0000

    dates in Js

---
+++ 01_basics/06_DatesinJs.js
@@ -0,0 +1,22 @@
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+ 
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toJSON());
+
+//  let myCreatedDate = new Date(2003, 8, 18, 5, 3);
+
+let myCreatedDate = new Date("2023-09-18");
+//  console.log(myCreatedDate.toLocaleString());
+
+/******* Time stamps ****/
+
+let mytimestamp = Date.now()
+
+console.log(mytimestamp);
+
+
+
