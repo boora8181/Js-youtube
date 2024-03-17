2805c6822ae1d134f35b1ce0d23257939f7f75af
Aman Boora <146092218+boora8181@users.noreply.github.com>
2024-03-17 10:17:39 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,12 +7,21 @@ const Jsuser= {
     location  : "Haryana",
     isloggedin : "false",
 }
-console.log(Jsuser);
+// console.log(Jsuser);
 // console.log(Jsuser.email);
 // console.log(Jsuser["email"]);
 
 Jsuser.email = "aman@123.com"
 Object.freeze(Jsuser)   // freeze property use karne ke baad object me changes show nhi hote kaise hamne jsuser ko freeze karke usk email change kari lekin email me koi change nhi aaya
 Jsuser.email = "boora@1234.com"
-console.log(Jsuser.email);
+// console.log(Jsuser.email);
 
+
+const course = {
+    coursename : ' Js in hindi', 
+    price : '999', 
+    Instructor : 'Hitesh'
+     
+}
+
+console.log(course.price);
