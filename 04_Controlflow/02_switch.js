bf1a2fdf8457d62d401a0ad5724468628cc7313a
Aman Boora <146092218+boora8181@users.noreply.github.com>
2024-03-19 12:01:44 +0000

    control flow started

---
+++ 04_Controlflow/02_switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month =3
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case matched");
+        break;
+}
---
+++ 04_Controlflow/1_if.js
@@ -0,0 +1,49 @@
+// If 
+
+const temperature = 41
+
+// if (temperature == 52) {
+//     console.log(" temperature is less than 50");
+
+// }
+// else {
+//     console.log(" temperature is greater than 50");
+// }
+
+// <, >, =<, =>, ==, ===, !=(not equal), !==
+
+
+const Balance = 1000
+
+// if (Balance < 500) {
+//     console.log("less than 500");
+
+// } else if (Balance < 750){
+//      console.log("less than 750");
+// } else if (Balance < 900){
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1200");
+// } 
+
+// 
+
+ const userloggedin = true
+const userdebitcard =  true
+const loggedinfromgoogle = true
+const loggedinfromemail = false
+
+if (userloggedin && userdebitcard) {           // yha pe && dono condition dekhta h 
+    console.log("allow user ");
+}
+if (loggedinfromgoogle || loggedinfromemail) {   // yha pe || sign or ka kaam karta h dono me se ek condition true honi chayiye 
+    console.log("user logged in");
+}
+
+
+
+
+
+
+
+
